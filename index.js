d21b44fce1d0b0805d258c441c11a118dfb4acb4
ChinmayDesai2005 <58367566+ChinmayDesai2005@users.noreply.github.com>
2021-06-21 09:14:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-let dateTime = new Date();
-let hours = dateTime.getHours();
-let min = dateTime.getMinutes();
-let sec = dateTime.getSeconds();
-let timeFormatted = String(hours) + ":" + String(min) + ":" + String(sec);
-let timeShow = document.getElementById('time')
-timeShow.innerHTML = timeFormatted;
-setTimeout(function(){
-    window.location.reload(1);
- }, 1000);
+function getTime(){
+    let dateTime = new Date();
+    let hours = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
+    let timeFormatted = String(hours) + ":" + String(min) + ":" + String(sec);
+    return timeFormatted;
+
+}
+function timeout(){
+    var refresh = 1000;
+    mytime = setTimeout('changeTime()', refresh);
+}
+function changeTime(){
+    let timeSend = getTime();
+    document.getElementById('time').innerHTML = timeSend;
+    timeout();
+}
+
+timeout();
